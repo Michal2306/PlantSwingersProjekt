cc57e2e312735fdc4fa32708e6c53fe887bad636
Michal Sitek <michal-sitek2306@wp.pl>
2020-08-21 20:36:20 +0200

    m

--- src/test/java/org/plantswingers/PlantSwingersAppTests.java
+++ src/test/java/org/plantswingers/PlantSwingersAppTests.java
@@ -1,10 +1,10 @@
-package org.planing.sbshoppongcart;
+package org.plantswingers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SbHibernateShoppingCartApplicationTests {
+class PlantSwingersAppTests {
 
 	@Test
 	void contextLoads() {
